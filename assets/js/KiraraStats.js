32bd519c81a95045696fb106320a5a80b813d7a7
asiaclonk <minh1111@gmx.de>
2020-02-11 04:16:43 +0100

    Update KiraraStats.js
    attempt to fix memory leak

--- assets/js/KiraraStats.js
+++ assets/js/KiraraStats.js
@@ -5,166 +5,167 @@ layout: blank
 $(document).ready(function() {
 	Chart.defaults.global.defaultFontColor = 'lightgray';
 	Chart.defaults.scale.gridLines.color = 'gray';
+
+	var json = {{ site.data.KirafanDB | jsonify }}
+	var cards = json.filter(value => value["series"] != "")
+	var fivestar = cards.filter(value => value["rarity"] == 5)
+	var series = [...new Set(cards.map(value => value["series"]))]
+
+	var objectlist1 = series.map(seriesname => ({ series: seriesname,
+												  cards: fivestar.filter(value => value["series"] == seriesname).length,
+												  weapons: fivestar.filter(value => value["series"] == seriesname && value["skill name en"] != "").length,
+												  evos: fivestar.filter(value => value["series"] == seriesname && value["skill evo name en"] != "").length,
+											    }))
+	objectlist1.sort(function(a,b) {
+		return b["cards"] - a["cards"] || b["weapons"] - a["weapons"] || b["evos"] - a["evos"];
+	})
 	
-	$("#button1").click(function() {
-		var json = {{ site.data.KirafanDB | jsonify }}
-		var fivestar = json.filter(value => value["rarity"] == 5 && value["series"] != "")
-		var series = [...new Set(fivestar.map(value => value["series"]))]
-		var objectlist = series.map(seriesname => ({ series: seriesname,
-													cards: fivestar.filter(value => value["series"] == seriesname).length,
-													weapons: fivestar.filter(value => value["series"] == seriesname && value["skill name en"] != "").length,
-													evos: fivestar.filter(value => value["series"] == seriesname && value["skill evo name en"] != "").length,
-													}))
-		objectlist.sort(function(a,b) {
-			return b["cards"] - a["cards"] || b["weapons"] - a["weapons"] || b["evos"] - a["evos"];
-		})
-		var data = {
-		  labels: objectlist.map(value => value["series"]),
-		  datasets: [{
-			label: "Evo 4",
-			backgroundColor: '#FFA500BB',
-			data: objectlist.map(value => value["evos"]),
-			yAxisID: "evoaxis",
-		  }, {
-			label: "Weapons",
-			backgroundColor: '#ADD8E6BB',
-			data: objectlist.map(value => value["weapons"]),
-			yAxisID: "weaponaxis",
-		  }, {
-			label: "Cards",
-			backgroundColor: '#4682B4BB',
-			data: objectlist.map(value => value["cards"]),
-			yAxisID: "cardaxis",
-		  }]
-		};
+	var objectlist2 = series.map(seriesname => ({ series: seriesname,
+												  five: cards.filter(value => value["series"] == seriesname && value["rarity"] == 5).length,
+												  four: cards.filter(value => value["series"] == seriesname && value["rarity"] == 4).length,
+												  three: cards.filter(value => value["series"] == seriesname && value["rarity"] == 3).length,
+												}))
+	objectlist2.sort(function(a,b) {
+		return b["five"] - a["five"] || b["four"] - a["four"] || b["three"] - a["three"];
+	})
+	
+	data1 = {
+	  labels: objectlist1.map(value => value["series"]),
+	  datasets: [{
+		label: "Evo 4",
+		backgroundColor: '#FFA500BB',
+		data: objectlist1.map(value => value["evos"]),
+		yAxisID: "evoaxis",
+	  }, {
+		label: "Weapons",
+		backgroundColor: '#ADD8E6BB',
+		data: objectlist1.map(value => value["weapons"]),
+		yAxisID: "weaponaxis",
+	  }, {
+		label: "Cards",
+		backgroundColor: '#4682B4BB',
+		data: objectlist1.map(value => value["cards"]),
+		yAxisID: "cardaxis",
+	  }]
+	};
 
-		var options = {
-		  maintainAspectRatio: false,
-		  layout: {
-			padding: {
-			  left: 200
-			}
+	options1 = {
+	  maintainAspectRatio: false,
+	  layout: {
+		padding: {
+		  left: 200
+		}
+	  },
+	  title: {
+		display: true,
+		text: '5* Release State Sistribution'
+	  },
+	  scales: {
+		xAxes: [{
+		  ticks: {
+			beginAtZero: true
 		  },
-		  title: {
-			display: true,
-			text: '5* release state distribution'
-		  },
-		  scales: {
-			xAxes: [{
-			  ticks: {
-				beginAtZero: true
-			  },
-			  stacked: false
-			}],
-			yAxes: [{
-			  id: "cardaxis",
-			  stacked: true,
-			}, {
-			  id: "weaponaxis",
-			  stacked: true,
-			  display: false,
-			  offset: true,
-			  type: "category",
-			  categoryPercentage: 0.8,
-			  barPercentage: 0.9,
-			}, {
-			  id: "evoaxis",
-			  stacked: true,
-			  display: false,
-			  offset: true,
-			  type: "category",
-			  categoryPercentage: 0.8,
-			  barPercentage: 0.9,
-			}]
-		  }
-		};
+		  stacked: false
+		}],
+		yAxes: [{
+		  id: "cardaxis",
+		  stacked: true,
+		}, {
+		  id: "weaponaxis",
+		  stacked: true,
+		  display: false,
+		  offset: true,
+		  type: "category",
+		  categoryPercentage: 0.8,
+		  barPercentage: 0.9,
+		}, {
+		  id: "evoaxis",
+		  stacked: true,
+		  display: false,
+		  offset: true,
+		  type: "category",
+		  categoryPercentage: 0.8,
+		  barPercentage: 0.9,
+		}]
+	  }
+	};
 
-		var ctx = document.getElementById("myChart").getContext("2d");
-		var myBarChart = new Chart(ctx, {
+	data2 = {
+	  labels: objectlist2.map(value => value["series"]),
+	  datasets: [{
+		label: "5*",
+		backgroundColor: '#FFD700BB',
+		data: objectlist2.map(value => value["five"]),
+		yAxisID: "fiveaxis",
+	  }, {
+		label: "4*",
+		backgroundColor: '#C0C0C0BB',
+		data: objectlist2.map(value => value["four"]),
+		yAxisID: "fouraxis",
+	  }, {
+		label: "3*",
+		backgroundColor: '#D2691EBB',
+		data: objectlist2.map(value => value["three"]),
+		yAxisID: "threeaxis",
+	  }]
+	};
+
+	options2 = {
+	  maintainAspectRatio: false,
+	  layout: {
+		padding: {
+		  left: 200
+		}
+	  },
+	  title: {
+		display: true,
+		text: 'Card rarity distribution'
+	  },
+	  scales: {
+		xAxes: [{
+		  ticks: {
+			beginAtZero: true
+		  },
+		  stacked: true
+		}],
+		yAxes: [{
+		  id: "fiveaxis",
+		  stacked: true,
+		}, {
+		  id: "fouraxis",
+		  stacked: true,
+		  display: false,
+		  offset: true,
+		  type: "category",
+		  categoryPercentage: 0.8,
+		  barPercentage: 0.9,
+		}, {
+		  id: "threeaxis",
+		  stacked: true,
+		  display: false,
+		  offset: true,
+		  type: "category",
+		  categoryPercentage: 0.8,
+		  barPercentage: 0.9,
+		}]
+	  }
+	};
+
+	kirarachart = new Chart($("#myChart").getContext("2d"),{
 		  type: 'horizontalBar',
-		  data: data,
-		  options: options,
+		  data: data1,
+		  options: options1,
 		});
+
+	$("#button1").click(function() {
+		kirarachart.type = 'horizontalBar';
+		kirarachart.data = data1;
+		kirarachart.options = options1;
 	});
 	
-		$("#button2").click(function() {
-		var json = {{ site.data.KirafanDB | jsonify }}
-		var cards = json.filter(value => value["series"] != "")
-		var series = [...new Set(cards.map(value => value["series"]))]
-		var objectlist = series.map(seriesname => ({ series: seriesname,
-													five: cards.filter(value => value["series"] == seriesname && value["rarity"] == 5).length,
-													four: cards.filter(value => value["series"] == seriesname && value["rarity"] == 4).length,
-													three: cards.filter(value => value["series"] == seriesname && value["rarity"] == 3).length,
-													}))
-		objectlist.sort(function(a,b) {
-			return b["five"] - a["five"] || b["four"] - a["four"] || b["three"] - a["three"];
-		})
-		var data = {
-		  labels: objectlist.map(value => value["series"]),
-		  datasets: [{
-			label: "5*",
-			backgroundColor: '#FFD700BB',
-			data: objectlist.map(value => value["five"]),
-			yAxisID: "fiveaxis",
-		  }, {
-			label: "4*",
-			backgroundColor: '#C0C0C0BB',
-			data: objectlist.map(value => value["four"]),
-			yAxisID: "fouraxis",
-		  }, {
-			label: "3*",
-			backgroundColor: '#D2691EBB',
-			data: objectlist.map(value => value["three"]),
-			yAxisID: "threeaxis",
-		  }]
-		};
-
-		var options = {
-		  maintainAspectRatio: false,
-		  layout: {
-			padding: {
-			  left: 200
-			}
-		  },
-		  title: {
-			display: true,
-			text: 'Card rarity distribution'
-		  },
-		  scales: {
-			xAxes: [{
-			  ticks: {
-				beginAtZero: true
-			  },
-			  stacked: true
-			}],
-			yAxes: [{
-			  id: "fiveaxis",
-			  stacked: true,
-			}, {
-			  id: "fouraxis",
-			  stacked: true,
-			  display: false,
-			  offset: true,
-			  type: "category",
-			  categoryPercentage: 0.8,
-			  barPercentage: 0.9,
-			}, {
-			  id: "threeaxis",
-			  stacked: true,
-			  display: false,
-			  offset: true,
-			  type: "category",
-			  categoryPercentage: 0.8,
-			  barPercentage: 0.9,
-			}]
-		  }
-		};
-
-		var ctx = document.getElementById("myChart").getContext("2d");
-		var myBarChart = new Chart(ctx, {
-		  type: 'horizontalBar',
-		  data: data,
-		  options: options,
-		});
+	$("#button2").click(function() {
+		kirarachart.type = 'horizontalBar';
+		kirarachart.data = data2;
+		kirarachart.options = options2;
 	});
 })
