33b92d9f05d2967e9b7821062dd08e1bda7b5c90
asiaclonk <minh1111@gmx.de>
2020-02-11 03:43:32 +0100

    second chart added

--- KiraraStats.md
+++ KiraraStats.md
@@ -2,13 +2,14 @@
 title: Kirara Fantasia Charts
 ---
 
-# JCharts Test
+# Some charts that
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 <script src="assets/js/KiraraStats.js"></script>
 <div style="text-align:center">
 	<button id="button1">5* release state</button>
+	<button id="button2">rarity distribution</button>
 </div>
-<div style="width: 100%; height: 400px; margin: 20px auto;">
+<div style="width: 100%; height: 550px; margin: 20px auto;">
 	<canvas id="myChart"></canvas>
 </div>
-# JCharts Test End
--- assets/js/KiraraStats.js
+++ assets/js/KiraraStats.js
@@ -3,6 +3,8 @@ layout: blank
 ---
 
 $(document).ready(function() {
+	Chart.defaults.global.defaultFontColor = 'lightgray';
+	
 	$("#button1").click(function() {
 		var json = {{ site.data.KirafanDB | jsonify }}
 		var fivestar = json.filter(value => value["rarity"] == 5 && value["series"] != "")
@@ -37,6 +39,11 @@ $(document).ready(function() {
 
 		var options = {
 		  maintainAspectRatio: false,
+		  layout: {
+			padding: {
+			  left: 70
+			}
+		  },
 		  title: {
 			display: true,
 			text: '5* release state distribution'
@@ -61,6 +68,87 @@ $(document).ready(function() {
 			  barPercentage: 0.9,
 			}, {
 			  id: "evoaxis",
+			  stacked: true,
+			  display: false,
+			  offset: true,
+			  type: "category",
+			  categoryPercentage: 0.8,
+			  barPercentage: 0.9,
+			}]
+		  }
+		};
+
+		var ctx = document.getElementById("myChart").getContext("2d");
+		var myBarChart = new Chart(ctx, {
+		  type: 'horizontalBar',
+		  data: data,
+		  options: options,
+		});
+	});
+	
+		$("#button2").click(function() {
+		var json = {{ site.data.KirafanDB | jsonify }}
+		var cards = json.filter(value => value["series"] != "")
+		var series = [...new Set(cards.map(value => value["series"]))]
+		var objectlist = series.map(seriesname => ({ series: seriesname,
+													five: cards.filter(value => value["series"] == seriesname && value["rarity"] == 5).length,
+													four: cards.filter(value => value["series"] == seriesname && value["rarity"] == 4).length,
+													three: cards.filter(value => value["series"] == seriesname && value["rarity"] == 3).length,
+													}))
+		objectlist.sort(function(a,b) {
+			return b["five"] - a["five"] || b["four"] - a["four"] || b["three"] - a["three"];
+		})
+		var data = {
+		  labels: objectlist.map(value => value["series"]),
+		  datasets: [{
+			label: "5*",
+			backgroundColor: '#FFD700BB',
+			data: objectlist.map(value => value["five"]),
+			yAxisID: "fiveaxis",
+		  }, {
+			label: "4*",
+			backgroundColor: '#C0C0C0BB',
+			data: objectlist.map(value => value["four"]),
+			yAxisID: "fouraxis",
+		  }, {
+			label: "3*",
+			backgroundColor: '#D2691EBB',
+			data: objectlist.map(value => value["three"]),
+			yAxisID: "threeaxis",
+		  }]
+		};
+
+		var options = {
+		  maintainAspectRatio: false,
+		  layout: {
+			padding: {
+			  left: 70
+			}
+		  },
+		  title: {
+			display: true,
+			text: 'Card rarity distribution'
+		  },
+		  scales: {
+			xAxes: [{
+			  ticks: {
+				beginAtZero: true
+			  },
+			  stacked: true
+			}],
+			yAxes: [{
+			  id: "fiveaxis",
+			  stacked: true,
+			}, {
+			  id: "fouraxis",
+			  stacked: true,
+			  display: false,
+			  offset: true,
+			  type: "category",
+			  categoryPercentage: 0.8,
+			  barPercentage: 0.9,
+			}, {
+			  id: "threeaxis",
 			  stacked: true,
 			  display: false,
 			  offset: true,
