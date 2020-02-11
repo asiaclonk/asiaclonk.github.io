7403380cb6dee55afa8bb57e5171d1bfd30f2ca7
asiaclonk <minh1111@gmx.de>
2020-02-11 02:48:45 +0100

    Update KiraraStats.js
    with sorting

--- assets/js/KiraraStats.js
+++ assets/js/KiraraStats.js
@@ -7,25 +7,26 @@ $(document).ready(function() {
 		var json = {{ site.data.KirafanDB | jsonify }}
 		var fivestar = json.filter(value => value["rarity"] == 5 && value["series"] != "")
 		var series = [...new Set(fivestar.map(value => value["series"]))]
-		var cards = series.map(seriesname => fivestar.filter(value => value["series"] == seriesname).length)
-		var weapons = series.map(seriesname => fivestar.filter(value => value["series"] == seriesname && value["skill name en"] != "").length)
-		var evos = series.map(seriesname => fivestar.filter(value => value["series"] == seriesname && value["skill evo name en"] != "").length)
+		var objectlist = series.map(seriesname => { "series": seriesname, "cards": fivestar.filter(value => value["series"] == seriesname).length, "weapons": fivestar.filter(value => value["series"] == seriesname && value["skill name en"] != "").length, "evos": fivestar.filter(value => value["series"] == seriesname && value["skill evo name en"] != "").length })
+		objectlist.sort(function(a,b) {
+			return b["cards"] - a["cards"] || b["weapons"] - a["weapons"] || b["evos"] - a["evos"];
+		})
 		var data = {
-		  labels: series,
+		  labels: objectlist.map(value => value["series"]),
 		  datasets: [{
 			label: "Evo 4",
 			backgroundColor: '#FFA500BB',
-			data: evos,
+			data: objectlist.map(value => value["evos"]),
 			yAxisID: "evoaxis",
 		  }, {
 			label: "Weapons",
 			backgroundColor: '#ADD8E6BB',
-			data: weapons,
+			data: objectlist.map(value => value["weapons"]),
 			yAxisID: "weaponaxis",
 		  }, {
 			label: "Cards",
 			backgroundColor: '#4682B4BB',
-			data: cards,
+			data: objectlist.map(value => value["cards"]),
 			yAxisID: "cardaxis",
 		  }]
 		};
