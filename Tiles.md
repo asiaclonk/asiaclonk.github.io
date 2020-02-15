9ce408b5244cbc1cf5885117942f0ee4307b46c2
asiaclonk <minh1111@gmx.de>
2020-02-15 19:06:03 +0100

    tiles

--- Tiles.md
+++ Tiles.md
@@ -7,16 +7,16 @@ published: false
 # For a certain Factorio fan
 
 <script src="assets/js/Tiles.js"></script>
-<div style="display: none">
-	<image id="tilebackground" src="assets/images/tilebackground.png"></image>
-</div>
 <div style="text-align: center; width: 384px; height: 64px">
 	<image id="tiles" src="assets/images/tiles.png" class="backgroundimage"></image>
 	<canvas id="selectmap" class="foregroundimage"></canvas>
 </div>
 
-<div style="width: 100%; height: 640px; margin: 20px auto;">
-	<canvas id="background" class="backgroundimage"></canvas>
-	<canvas id="tilemap" class="foregroundimage"></canvas>
-	<canvas id="foreground" class="forestgroundimage"></canvas>
+<div style="width: 100%; height: 700px; overflow: auto">
+	<div style="width: 640px; height: 640px; margin: 20px auto;">
+		<image id="backgroundtiles" src="assets/images/tilebackground.png" style="display: none;"></image>
+		<canvas id="background" class="backgroundimage"></canvas>
+		<canvas id="tilemap" class="foregroundimage"></canvas>
+		<canvas id="foreground" class="forestgroundimage"></canvas>
+	</div>
 </div>
--- assets/js/Tiles.js
+++ assets/js/Tiles.js
@@ -1,13 +1,94 @@
 $(document).ready(function() {
-	tileh = 32
-	tilew = 32
+	tilelist = [];
+	dim = 32;
 	selectedx = 0;
 	selectedy = 0;
+	xmappix = 0;
+	ymappix = 0;
+	dragx = 0;
+	dragy = 0;
+	mapclick = false;
+	isdragging = false;
+	tiles = document.getElementById("tiles");
+	backgroundtiles = document.getElementById("backgroundtiles");
+	
+	selectcanvas = document.getElementById("selectmap");
+	selectcontext = selectcanvas.getContext("2d");
+	mapdim = 640;
+	backcontext = backcanvas.getContext("2d");
+	tilecontext = tilecontext.getContext("2d");
+	drawcontext = drawcontext.getContext("2d");
 
-	$("#selectmap").mouseover(function() {
-		var tilescontext = document.getElementById("selectmap").getContext("2d");
-		var relativex = 0;
-		var relativey = 0;
-		tilescontext.drawImage($("#tiles"));
+	$("#selectmap").mouseover(function(e) {
+		var hoverx = Math.floor(e.clientX / dim);
+		var hovery = Math.floor(e.clientY / dim);
+		selectcontext.clearRect(0, 0, selectcanvas.width, selectcanvas.height);
+		selectcontext.beginPath();
+		selectcontext.strokeStyle = 'red';
+		selectcontext.rect(hoverx * dim, hovery * dim, dim, dim);
+		selectcontext.stroke();
+		drawselection();
+	});
+	
+	$("#selectmap").click(function(e) {
+		selectedx = Math.floor(e.clientX / dim);
+		selectedy = Math.floor(e.clientY / dim);
+		drawselection();
+	});
+	
+	$("#backgroundtiles").on("load", function() {
+		backcontext.fillStyle = backcontext.createPattern(backgroundtiles, "repeat");
+		backcontext.fillRect(0, 0, mapdim, mapdim);
+	});
+	
+	$("#foreground").mousedown(function(e) {
+		dragx = e.clientX;
+		dragy = e.clientY;
+		mapclick = true;
+	});
+	
+	$("#foreground").mousemove(function(e) {
+		if (mapclick == true) {
+			backcontext.setTransform(xmappix + (e.clientX - dragx) % 512, ymappix + (e.clientY - dragy) % 32);
+			drawmap();
+		}
+	});
+	
+	$("#foreground").mouseup(function(e) {
+		if (dragx == e.clientX && dragy = e.clientY) {
+			var selectedmapx = Math.floor((xmappix + e.clientX) / 32);
+			var selectedmapy = Math.floor((ymappix + e.clientY) / 32);
+			if (selectedx == 0 && selectedy == 0) {
+				tilelist = tilelist.filter(value => value.mapx != selectedmapx || value.mapy != selectedmapy);
+			}
+			else {
+				tilelist.push({ tilex: selectedx , tiley: selectedy , mapx: selectedmapx, mapy: selectedmapy })
+			}
+			drawmap();
+		}
+		mapclick = false;
+		xmappix += (e.clientX - dragx);
+		ymappix += (e.clientY - dragy);
+		dragx = 0;
+		dragy = 0;
+	});
+}
+
+function drawselection() {
+	context.beginPath();
+	context.strokeStyle = 'green';
+	context.rect(selectedx * dim, selectedy * dim, dim, dim);
+	context.stroke();
+}
+
+function drawmap() {
+	tilecontext.clearRect(0, 0, mapdim, mapdim);
+	var xmap = Math.floor(xmappix / 32);
+	var xmapmax = Math.floor((xmappix + 640) / 32);
+	var ymap = ymappix / 32;
+	var ymapmax = Math.floor((ymappix + 640) / 32);
+	var tilestodraw = tilelist.filter(value => value.mapx >= xmap && value.mapx <= xmapmax && value.mapy >= ymap && value.mapy <= ymapmax);
+	tilestodraw.forEach(tile => {
+		tilecontext.drawImage(tiles, (tile.mapx - xmap) * 32 - (xmap % 32), (tile.mapy - ymap) * 32 - (ymap % 32), dim, dim, tile.tilex * 32, tile.tile.y * 32, dim, dim);
 	})
 }
